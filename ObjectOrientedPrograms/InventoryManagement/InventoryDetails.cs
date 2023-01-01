caa5587258325dc629a6a7cc2f7374b1572c9676
pkr187 <pankajroman187@gmail.com>
2023-01-01 22:57:12 +0530

    [pankaj] Add:InventoryDataManagement

---
+++ ObjectOrientedPrograms/InventoryManagement/InventoryDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPrograms.InventoryManagement
+{
+    public class InventoryDetails
+    {
+        public string Name { get; set; }
+        public int weight { get; set; }
+        public int price { get; set; }
+    }
+}
+    
+
---
+++ ObjectOrientedPrograms/Program.cs
@@ -0,0 +1,25 @@
+﻿using ObjectOrientedPrograms.InventoryManagement;
+
+namespace ObjectOrientedPrograms
+{
+    internal class Program
+    {
+        //given inventory path to json
+        const string INVENTORY_DATA_FILE_PATH = @"C:\Object Oriented Practice Problem\ObjectOrientedPrograms\InventoryManagement\Inventory.json";
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Select Programs\n 1.InventoryManagement");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Inventory inventory = new Inventory();
+                        inventory.ReadJsonFile(INVENTORY_DATA_FILE_PATH);
+                        break;
+                }
+            }
+        }
+    }
+}
